9119cfc9189120b05d410d97512b8aa28e4ab5b6
Mohith PANATI VENKAT (contractor) <mohith.panativenkat@amadeus.com>
2017-08-21 20:38:51 +0530

    Initial commit

---
+++ src/mapreduce/demo/task1/Task1Mapper.java
@@ -0,0 +1,28 @@
+package mapreduce.demo.task1;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.*; 
+
+public class Task1Mapper extends Mapper<Text, Text, Text, Text> {
+	@SuppressWarnings("null")
+	public void map(Text key, Text value, Context context) 
+			throws IOException, InterruptedException {
+		String[] lineArray = value.toString().split("\n");
+		
+		Text emptyText = null;
+		emptyText.set(new String("")) ;
+
+		for(String line: lineArray){
+		boolean companyNameNull = line.split("|")[0].equalsIgnoreCase("NA");
+		boolean productNameNull = line.split("|")[1].equalsIgnoreCase("NA");
+		if(companyNameNull || productNameNull){
+			emptyText.set(emptyText.toString() + line);
+		}
+		}
+		context.write(key, emptyText);
+	}
+}
